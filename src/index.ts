24ca908b4b28d7175811b9af5f21ac78c9488678
Ahmed Ali <imahmedalibhatti@gmail.com>
2022-07-22 13:19:41 +0500

    feat: add headers code

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,34 @@
-const foo: string = "Hello World";
-console.log(foo);
+#! /usr/bin/env node
+import clipboard from "clipboardy";
+import { Command } from "commander";
+
+const program = new Command();
+
+program.name("sol-headers").description("CLI to create solidity headers").version("1.0.0");
+
+program
+  .command("create", { isDefault: true })
+  .description("Create solidity headers")
+  .argument("<string>", "heading")
+  .action(input => {
+    let space = "";
+
+    while (64 - (space.length + input.length) > space.length) {
+      space += " ";
+    }
+
+    const output =
+      "    /*//////////////////////////////////////////////////////////////" +
+      "\n" +
+      "    " +
+      space +
+      input.toUpperCase() +
+      "\n" +
+      "    //////////////////////////////////////////////////////////////*/";
+
+    console.log(output);
+
+    clipboard.writeSync(output);
+  });
+
+program.parse();
